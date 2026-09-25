eff17320b0455847412185f39a90f43dc3e03483
Kathleen Saz <kathleen-saz@users.noreply.github.com>
2026-09-25 14:08:49 -0300

    Fix WBCcad wordmark

--- components/visuals.tsx
+++ components/visuals.tsx
@@ -1,11 +1,8 @@
 export function Wordmark() {
   return (
-    <svg className="wordmark" viewBox="0 0 152 28" role="img" aria-label="WBCcad">
-      <path d="M1 4h5l5 17 5-17h4l5 17 5-17h5l-8 23h-4l-5-15-5 15H9L1 4Z" fill="currentColor" />
-      <path d="M39 4h11c5 0 8 2 8 6 0 2-1 4-3 5 3 1 4 3 4 6 0 4-3 7-9 7H39V4Zm5 4v5h6c2 0 3-1 3-3s-1-2-3-2h-6Zm0 9v6h6c3 0 4-1 4-3s-1-3-4-3h-6Z" fill="currentColor" />
-      <path d="M73 3c5 0 9 2 11 6l-4 2c-1-2-4-4-7-4-5 0-8 3-8 8s3 9 8 9c3 0 6-2 7-5l4 2c-2 4-6 7-11 7-8 0-13-5-13-13S65 3 73 3Z" fill="currentColor" />
-      <path d="M87 12h3v13c0 2 0 2 2 2h1v3h-2c-3 0-4-1-4-4V12Zm2-6a2 2 0 1 1 0 4 2 2 0 0 1 0-4Zm13 5c4 0 7 2 8 5h-4c0-1-2-2-4-2-3 0-5 2-5 5s2 5 5 5c2 0 3-1 4-3h4c-1 4-4 6-8 6-5 0-9-3-9-8s4-8 9-8Zm16 0c5 0 8 3 8 8v8h-4v-2c-1 2-3 2-5 2-4 0-6-2-6-5 0-4 3-5 7-5h4c0-2-2-3-4-3s-3 1-4 2l-3-2c2-2 4-3 7-3Zm0 9c-2 0-3 1-3 2s1 2 3 2c3 0 4-2 4-4h-4Zm22-15h4v22h-4v-2c-1 2-3 2-5 2-5 0-8-3-8-8s3-8 8-8c2 0 4 1 5 2V5Zm-4 9c-3 0-5 2-5 5s2 5 5 5 4-2 4-5-1-5-4-5Z" fill="currentColor" />
-    </svg>
+    <span className="wordmark" aria-label="WBCcad">
+      <b>WBC</b><span>cad</span>
+    </span>
   );
 }
 
